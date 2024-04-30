9fae9370f825d404adef66e9196a614c680cb3a4
arti <arti@gmail.com>
2024-04-30 23:37:20 +0530

    adding header component

--- projects/lib-shared-modules/src/lib/components/header/header.component.ts
+++ projects/lib-shared-modules/src/lib/components/header/header.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lib-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @Input() backButton : boolean = true
 
+  constructor(private router : Router) {}
+
+  backArrowButton() {
+    this.router.navigate(['']);
+    console.log('onclickbackbutton')
+  }
 }
