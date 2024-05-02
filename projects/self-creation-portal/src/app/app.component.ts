8229ae137a6930e4cf0d7c3c61261e48ed5b9a28
GSuma21 <21gsuma@gmail.com>
2024-05-02 15:24:18 +0530

    issue #feat tailwind

--- projects/self-creation-portal/src/app/app.component.ts
+++ projects/self-creation-portal/src/app/app.component.ts
@@ -1,16 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from 'lib-shared-modules';
-
-
+import { HeaderComponent, CardComponent } from 'lib-shared-modules';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+// import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,MatCardModule,CardComponent,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  list:any = 
+    {
+        "id": 4,
+        "title": "sample project",
+        "type": "project",
+        "organization": {
+            "id": 24,
+            "name": "Tunerlabs",
+            "code": "tl"
+        },
+        "status": "DRAFT",
+        "actionButton":[{action:'VIEW',label:'View'},{ action:'EDIT',label:'Edit'}]
+    }
+
+    trackByFn(index: number, item: any): any {
+      return index;
+    }
+    heroes:any = [
+      {name:"Suma"},
+      {name:"NN"}
+    ]
 
 }
