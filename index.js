15e6c17fca93f9e9ada79e8a44b2d23fef482e59
ramkumar-pacewisdom <ramkumar.n@pacewisdom.com>
2024-06-11 17:48:06 +0530

    bundle file directory changes

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 1590;
 
-app.use(express.static(path.join(__dirname, 'www')));
+app.use(express.static(path.join(__dirname+"/dist/self-creation-portal", 'browser')));
 
 app.get('*', (req, res) => {
     // res.sendFile(path.join(__dirname, 'www', 'index.html'));
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname+"/dist/self-creation-portal",'index.html'));
   });
 
 app.listen(port, () => {
