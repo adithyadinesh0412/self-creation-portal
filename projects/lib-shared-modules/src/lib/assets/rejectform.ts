b1a1a6bb10ef070f1bbbe64473261af192cdf37a
GSuma21 <21gsuma@gmail.com>
2024-08-23 12:33:56 +0530

    issue #fix

--- projects/lib-shared-modules/src/lib/assets/rejectform.ts
+++ projects/lib-shared-modules/src/lib/assets/rejectform.ts
@@ -12,7 +12,7 @@ export const rejectform = {
       "maxLength": "Reason must not exceed 256 characters"
     },
     "validators": {
-      "required": true,
+      "required": false,
       "maxLength": 255,
       "pattern": "^(?! )(?!.* {3})[\\p{L}a-zA-Z0-9\\-\\_\\- <>&]+$"
     }
