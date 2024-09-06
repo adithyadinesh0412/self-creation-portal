9811681b4bdb068d9349521f1f74e4b3e4c4c7fb
GSuma21 <21gsuma@gmail.com>
2024-09-06 17:29:53 +0530

    added fix for opening creator message and solved error in comment saving

--- projects/lib-shared-modules/src/lib/components/comments-box/comments-box.component.ts
+++ projects/lib-shared-modules/src/lib/components/comments-box/comments-box.component.ts
@@ -123,11 +123,11 @@ export class CommentsBoxComponent implements OnInit, OnDestroy {
     }
     this.comment.emit(this.quillInput)
     this.commentPayload.parent_id= this.messages.length > 0 ? this.messages[this.messages.length-1].id : 0;
-    if(this.draft) {
+    if(this.draft && this.quillInput) {
       this.draft.text = this.quillInput;
       this.utilService.updateComment(this.resourceId,this.draft,this.draft.id).subscribe((res) => console.log(res));
     }
-    else {
+    else if(this.quillInput){
       this.commentPayload.text = this.quillInput;
       this.utilService.updateComment(this.resourceId,this.commentPayload).subscribe((res:any) => {
         this.draft = res.result;
